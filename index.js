64c2650dbc50acc610e429f05015bb3138a231e9
Caroline Wangari <karehwachira@gmail.com>
2023-12-12 10:40:01 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = () => {
+    return 2000 / 100;
+  };
+  
+  // Arrow function called square
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  };
